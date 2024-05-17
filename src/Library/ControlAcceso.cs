77c1312e701469dcdd0b3700b79ae65314c9518b
AgustinNegreira <agustin.negreira@correo.ucu.edu.uy>
2024-05-17 00:12:12 -0300

    ControlAcceso

---
+++ src/Library/ControlAcceso.cs
@@ -0,0 +1,14 @@
+﻿namespace Library
+{
+    public class ControlAcceso
+    {
+        public Thread CamaraEntrada { get; set; }
+        public Thread CamaraCantina { get; set; }
+        public Thread CamaraPasillo { get; set; }
+
+        public void IniciarReconocimiento()
+        {
+            
+        }
+    }
+}
