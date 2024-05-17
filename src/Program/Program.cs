01d36c3f88fe582d930699238c7d835ed59100e5
Bmorat <brian.morat@correo.ucu.edu.uy>
2024-05-17 11:37:41 -0300

    person

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Program;
+﻿using System.Text.Json.Serialization;
+using Library;
+using System.Text.Json;
+
+
+namespace Program;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        Person persona1=Person.GetRandomPerson();
+        Console.WriteLine($"hola soy {persona1.Nombre} {persona1.Apellido} mi id es {persona1.Id} y estoy {persona1.Estado}");
+
+
+        
     }
 }
